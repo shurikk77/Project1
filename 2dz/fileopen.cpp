84f41195cfd54a75a63c5d23011f4b245f613092
shurikk77 <71214529+shurikk77@users.noreply.github.com>
2021-03-14 23:54:26 +0300

    Add files via upload

---
+++ 2dz/fileopen.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <fstream> // подключаем библиотеку
+using namespace std;
+
+int main()
+{
+  string path = "test.txt";
+  ifstream file;
+  file.open(path);
+  if (!file.is_open())
+  {
+    cout << "cant open";
+  }
+  else
+  {
+    cout << "opened" << endl;
+    char ch;
+    while (file.get(ch))
+    {
+      cout << ch;
+    }
+  }
+  file.close();
+  return 0;
+}
